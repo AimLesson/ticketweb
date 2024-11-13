58d51813f3cd77352dd9876669a1e34ae036d760
Gerson Manuel Sugianto <manuelsugianto@gmail.com>
2024-11-13 10:41:06 +0700

    Update home.blade.php

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="icon" type="image/x-icon" href="/assets/Asset-23.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
 
